b827e449a32ceaeb7c661f461a031ecb574bcd08
Ulyses <ulyses.rico.2012@gmail.com>
2021-09-07 18:06:36 -0500

    ...

--- jobServiceApp/readFromExcel.py
+++ jobServiceApp/readFromExcel.py
@@ -1,3 +1,4 @@
+from numpy import nan
 import pandas as pd
 import openpyxl as excelpy
 
@@ -21,9 +22,16 @@ for index,row in excelDF.iterrows():
         if field=='id_thesis' or field=='period_number':
             strValue=int(row[field])
         elif field=='lst_precedents':
-            strValue=str(row[field]).replace(['[',']'],'')
+            #For this case, it adds a list to the current lsValue and it continues
+            strValue=str(row[field]).replace('[','').replace(']','').replace("'",'')
+        elif field=='multiple_subjects':
+            strValue=bool(row[field])
         else:        
             #String case
             strValue=row[field] 
+        if strValue==nan:
+            strValue=''    
         lsValue.append(strValue)
+    rowCount+=1
+    print(f'Row done {str(rowCount)}')    
    
