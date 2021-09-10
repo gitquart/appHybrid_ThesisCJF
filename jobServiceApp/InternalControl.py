4f6b39a189674c32ed559b023963915b27f1e6e8
Ulyses <ulyses.rico.2012@gmail.com>
2021-09-10 17:19:56 -0500

    ...

--- jobServiceApp/InternalControl.py
+++ jobServiceApp/InternalControl.py
@@ -4,7 +4,7 @@ class cInternalControl:
     idControl=4
     timeout=70
     hfolder='jobServiceApp' 
-    heroku=False
+    heroku=True
     rutaHeroku='/app/jobServiceApp/'
     rutaLocal=os.getcwd()+'\\jobServiceApp\\'
     download_dir=''
